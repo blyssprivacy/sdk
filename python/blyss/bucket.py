b77a93f43a5354c6cc9f89a2ad06c84a2f062c33
Neil Movva <neil@blyss.dev>
2023-03-27 18:03:38 -0700

    Python client: new AsyncBucket interface (#17)
    New client interface to support async usage. 
    This commit implements async versions of write() and privateRead(); all other calls fall back to blocking synchronous versions.

--- python/blyss/bucket.py
+++ python/blyss/bucket.py
@@ -9,8 +9,10 @@ from . import api, serializer, seed
 from .blyss_lib import BlyssLib
 
 import json
+import base64
 import bz2
 import time
+import asyncio
 
 
 def _chunk_parser(raw_data: bytes) -> Iterator[bytes]:
@@ -71,7 +73,98 @@ class Bucket:
             else:
                 raise e
 
-    def _private_read(self, keys: list[str]) -> list[tuple[bytes, Optional[dict[Any, Any]]]]:
+    async def _async_check(self, uuid: str) -> bool:
+        try:
+            await self.api.async_check(uuid)
+            return True
+        except api.ApiException as e:
+            if e.code == 404:
+                return False
+            else:
+                raise e
+
+    def _split_into_chunks(
+        self, kv_pairs: dict[str, bytes]
+    ) -> list[list[dict[str, str]]]:
+        _MAX_PAYLOAD = 5 * 2**20  # 5 MiB
+
+        # 1. Bin keys by row index
+        keys_by_index: dict[int, list[str]] = {}
+        for k in kv_pairs.keys():
+            i = self.lib.get_row(k)
+            if i in keys_by_index:
+                keys_by_index[i].append(k)
+            else:
+                keys_by_index[i] = [k]
+
+        # 2. Prepare chunks of items, where each is a JSON-ready structure.
+        # Each chunk is less than the maximum payload size, and guarantees
+        # zero overlap of rows across chunks.
+        kv_chunks: list[list[dict[str, str]]] = []
+        current_chunk: list[dict[str, str]] = []
+        current_chunk_size = 0
+        sorted_indices = sorted(keys_by_index.keys())
+        for i in sorted_indices:
+            keys = keys_by_index[i]
+            # prepare all keys in this row
+            row = []
+            row_size = 0
+            for key in keys:
+                value = kv_pairs[key]
+                value_str = base64.b64encode(value).decode("utf-8")
+                fmt = {
+                    "key": key,
+                    "value": value_str,
+                    "content-type": "application/octet-stream",
+                }
+                row.append(fmt)
+                row_size += int(72 + len(key) + len(value_str))
+
+            # if the new row doesn't fit into the current chunk, start a new one
+            if current_chunk_size + row_size > _MAX_PAYLOAD:
+                kv_chunks.append(current_chunk)
+                current_chunk = row
+                current_chunk_size = row_size
+            else:
+                current_chunk.extend(row)
+                current_chunk_size += row_size
+
+        # add the last chunk
+        if len(current_chunk) > 0:
+            kv_chunks.append(current_chunk)
+
+        return kv_chunks
+
+    def _generate_query_stream(self, keys: list[str]) -> bytes:
+        # generate encrypted queries
+        queries: list[bytes] = [
+            self.lib.generate_query(self.public_uuid, self.lib.get_row(k)) for k in keys
+        ]
+        # interleave the queries with their lengths (uint64_t)
+        query_lengths = [len(q).to_bytes(8, "little") for q in queries]
+        lengths_and_queries = [x for lq in zip(query_lengths, queries) for x in lq]
+        # prepend the total number of queries (uint64_t)
+        lengths_and_queries.insert(0, len(queries).to_bytes(8, "little"))
+        # serialize the queries
+        multi_query = b"".join(lengths_and_queries)
+        return multi_query
+
+    def _unpack_query_result(
+        self, keys: list[str], raw_result: bytes, parse_metadata: bool = True
+    ) -> list[bytes]:
+        retrievals = []
+        for key, result in zip(keys, _chunk_parser(raw_result)):
+            decrypted_result = self.lib.decode_response(result)
+            decompressed_result = bz2.decompress(decrypted_result)
+            extracted_result = self.lib.extract_result(key, decompressed_result)
+            if parse_metadata:
+                output = serializer.deserialize(extracted_result)
+            else:
+                output = extracted_result
+            retrievals.append(output)
+        return retrievals
+
+    def _private_read(self, keys: list[str]) -> list[bytes]:
         """Performs the underlying private retrieval.
 
         Args:
@@ -84,30 +177,13 @@ class Bucket:
             self.setup()
             assert self.public_uuid
 
-        # generate encrypted queries
-        queries: list[bytes] = [
-            self.lib.generate_query(self.public_uuid, self.lib.get_row(k)) 
-            for k in keys
-        ]
-        # interleave the queries with their lengths (uint64_t)
-        query_lengths = [len(q).to_bytes(8, "little") for q in queries]
-        lengths_and_queries = [x for lq in zip(query_lengths, queries) for x in lq]
-        # prepend the total number of queries (uint64_t)
-        lengths_and_queries.insert(0, len(queries).to_bytes(8, "little"))
-        # serialize the queries
-        multi_query = b"".join(lengths_and_queries)
-        
+        multi_query = self._generate_query_stream(keys)
+
         start = time.perf_counter()
         multi_result = self.api.private_read(self.name, multi_query)
         self.exfil = time.perf_counter() - start
 
-        retrievals = [] 
-        for key, result in zip(keys, _chunk_parser(multi_result)):
-            decrypted_result = self.lib.decode_response(result)
-            decompressed_result = bz2.decompress(decrypted_result)
-            extracted_result = self.lib.extract_result(key, decompressed_result)
-            output = serializer.deserialize(extracted_result)
-            retrievals.append(output)
+        retrievals = self._unpack_query_result(keys, multi_result)
 
         return retrievals
 
@@ -184,11 +260,11 @@ class Bucket:
 
         Args:
             keys (str): A key or list of keys to privately read.
-                        If a list of keys is supplied, 
+                        If a list of keys is supplied,
                         results will be returned in the same order.
 
         Returns:
-            bytes: The value found for the key in the bucket, 
+            bytes: The value found for the key in the bucket,
                    or None if the key was not found.
         """
         single_query = False
@@ -201,7 +277,6 @@ class Bucket:
             return results[0]
 
         return results
-
 
     def private_key_intersect(self, keys: list[str]) -> list[str]:
         """Privately intersects the given set of keys with the keys in this bucket,
@@ -217,3 +292,36 @@ class Bucket:
         bloom_filter = self.api.bloom(self.name)
         present_keys = list(filter(bloom_filter.lookup, keys))
         return present_keys
+
+
+class AsyncBucket(Bucket):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def write(self, kv_pairs: dict[str, bytes], MAX_CONCURRENCY=8):
+        # Split the key-value pairs into chunks not exceeding max payload size.
+        kv_chunks = self._split_into_chunks(kv_pairs)
+        # Make one write call per chunk, while respecting a max concurrency limit.
+        sem = asyncio.Semaphore(MAX_CONCURRENCY)
+
+        async def _paced_writer(chunk):
+            async with sem:
+                await self.api.async_write(self.name, json.dumps(chunk))
+
+        _tasks = [asyncio.create_task(_paced_writer(c)) for c in kv_chunks]
+        await asyncio.gather(*_tasks)
+
+    async def private_read(self, keys: list[str]) -> list[bytes]:
+        if not self.public_uuid or not await self._async_check(self.public_uuid):
+            self.setup()
+            assert self.public_uuid
+
+        multi_query = self._generate_query_stream(keys)
+
+        start = time.perf_counter()
+        multi_result = await self.api.async_private_read(self.name, multi_query)
+        self.exfil = time.perf_counter() - start
+
+        retrievals = self._unpack_query_result(keys, multi_result, parse_metadata=False)
+
+        return retrievals
